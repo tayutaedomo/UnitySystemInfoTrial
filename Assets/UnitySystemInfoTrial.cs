c7760c18e28a1c4cc1cddf18023db6dd0c5a3268
tayutaedomo <tayutaedomo@gmail.com>
2016-12-12 18:54:58 +0900

    Add project files

---
+++ Assets/UnitySystemInfoTrial.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnitySystemInfoTrial : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Application.platform: " + Application.platform);
+
+		Debug.Log ("SystemInfo.deviceType: " + SystemInfo.deviceType);
+		Debug.Log ("SystemInfo.operatingSystem: " + SystemInfo.operatingSystem);
+		Debug.Log ("SystemInfo.deviceUniqueIdentifier: " + SystemInfo.deviceUniqueIdentifier);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
