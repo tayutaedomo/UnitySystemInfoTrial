2501207cbb27609dcc9f180ef21a2c5642f42f18
tayutaedomo <tayutaedomo@gmail.com>
2016-12-12 20:32:19 +0900

    Add print code

--- Assets/UnitySystemInfoTrial.cs
+++ Assets/UnitySystemInfoTrial.cs
@@ -5,13 +5,17 @@ public class UnitySystemInfoTrial : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		Debug.Log ("Application.platform: " + Application.platform);
-
 		Debug.Log ("SystemInfo.deviceModel: " + SystemInfo.deviceModel);
 		Debug.Log ("SystemInfo.deviceName: " + SystemInfo.deviceName);
 		Debug.Log ("SystemInfo.deviceType: " + SystemInfo.deviceType);
-		Debug.Log ("SystemInfo.operatingSystem: " + SystemInfo.operatingSystem);
 		Debug.Log ("SystemInfo.deviceUniqueIdentifier: " + SystemInfo.deviceUniqueIdentifier);
+		Debug.Log ("SystemInfo.operatingSystem: " + SystemInfo.operatingSystem);
+
+		Debug.Log ("Application.bundleIdentifier: " + Application.bundleIdentifier);
+		Debug.Log ("Application.platform: " + Application.platform);
+		Debug.Log ("Application.productName: " + Application.productName);
+		Debug.Log ("Application.systemLanguage: " + Application.systemLanguage);
+		Debug.Log ("Application.version: " + Application.version);
 	}
 	
 	// Update is called once per frame
